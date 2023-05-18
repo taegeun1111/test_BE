4acaaa262e104d688d9489f64709dc8d983eb987
JeFeel <jaephil94@gmail.com>
2023-05-18 16:36:15 +0900

    BE/ Service mapper 객체 변경

--- src/main/java/com/mountain/doo/service/replyservice/ReviewReplyService.java
+++ src/main/java/com/mountain/doo/service/replyservice/ReviewReplyService.java
@@ -1,7 +1,7 @@
 package com.mountain.doo.service.replyservice;
 
 
-import com.mountain.doo.repository.replymapper.FeedReplyMapper;
+import com.mountain.doo.repository.replymapper.ReviewReplyMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ReviewReplyService {
 
-    private final FeedReplyMapper replyMapper;
+    private final ReviewReplyMapper replyMapper;
 
     // 댓글 목록 조회
 
