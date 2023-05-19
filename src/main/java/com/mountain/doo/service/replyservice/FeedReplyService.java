ad276d515dfb6722be978e578c56b702ba337dd4
JeFeel <jaephil94@gmail.com>
2023-05-19 11:04:44 +0900

    BE/ FeedReplyController, Service 작업중

--- src/main/java/com/mountain/doo/service/replyservice/FeedReplyService.java
+++ src/main/java/com/mountain/doo/service/replyservice/FeedReplyService.java
@@ -1,10 +1,15 @@
 package com.mountain.doo.service.replyservice;
 
 
+import com.mountain.doo.dto.page.Page;
+import com.mountain.doo.entity.Feed;
+import com.mountain.doo.entity.reply.FeedReply;
 import com.mountain.doo.repository.replymapper.FeedReplyMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -12,9 +17,13 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class FeedReplyService {
 
-    private final FeedReplyMapper replyMapper;
+    private final FeedReplyMapper replyMapper; //repository에 의존
 
     // 댓글 목록 조회
+    public List<FeedReply> getList(long feedBoardNo, Page page){
+//        List<FeedReply> replyList = replyMapper.findAll(feedBoardNo, page);
+        return replyMapper.findAll(feedBoardNo, page);
+    }
 
     // 댓글 등록
 
