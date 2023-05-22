a7dac82b10b5bbc39a462ab4fc4b18ea284c33e5
emilywin825 <emilywin825@naver.com>
2023-05-22 12:26:55 +0900

    파일업로드 통합

--- src/main/java/com/mountain/doo/service/AccountService.java
+++ src/main/java/com/mountain/doo/service/AccountService.java
@@ -105,10 +105,11 @@ public class AccountService {
         }
     }
 
-    public boolean save(Account account) {
+    public boolean save(Account account,final String savePath) {
         log.info("account: {}", account);
 
         account.setPassword(encoder.encode(account.getPassword()));
+        account.setProfileImg(savePath);
 
         return mapper.save(account);
     }
@@ -142,7 +143,7 @@ public class AccountService {
                 .build();
         //이 정보들을 세션에 저장
         session.setAttribute(LoginUtil.LOGIN_KEY,dto);
-
+        log.info("maintainAccountState에서 저장한 세션 : "+session.getAttribute(LoginUtil.LOGIN_KEY));
         // 세션의 수명을 설정 -> 1시간
         session.setMaxInactiveInterval(60 * 60);
 
@@ -176,6 +177,7 @@ public class AccountService {
         log.info("dbLoginTime등록여부2" + b);
         accountTrueFalse(b);
     }
+
     }
 
     public void accountTrueFalse(boolean b){ //true이면 로그인 스탬프 +1
--- src/test/java/com/mountain/doo/repository/AccountMapperTest.java
+++ src/test/java/com/mountain/doo/repository/AccountMapperTest.java
@@ -46,6 +46,7 @@ class AccountMapperTest {
         save.setGender(GENDER.M);
         save.setEmail("44");
         save.setPhoneNo("55");
+        save.setProfileImg("/2023/05/22/d69dc8ce-66bb-43ca-8c1a-0b6c1704a90b_bobby.png");
 
         boolean isSaved = mapper.save(save);
         assertTrue(isSaved);
