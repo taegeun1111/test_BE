d2fcbfa1b10ff5b7c6b0d682514bfc0f7f2829ae
oslob99 <jkh991116@naver.com>
2023-05-25 21:53:47 +0900

    #5/25 경훈 잠깐 왔다감

--- src/main/java/com/mountain/doo/controller/ReviewController.java
+++ src/main/java/com/mountain/doo/controller/ReviewController.java
@@ -62,7 +62,7 @@ public class ReviewController {
         log.info("detail에 접근한 User의 정보 : {}", request.getSession().getAttribute("login"));
         log.info("detail 게시물 정보 : {}",detail);
         ReviewLikeResponseDTO byAccountDTO = reviewService.findByAccount();
-
+        log.info("like 누른 회원들 계정 정보 출력 : {}",byAccountDTO);
         model.addAttribute("login", login);
         model.addAttribute("is", detail);
         model.addAttribute("l",byAccountDTO);
